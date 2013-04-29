769c2b507c9b0114eef06473340038c2e5c42f05
Stefan Boesen <stefan.boesen@gmail.com>
2013-04-29 11:59:21 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blargg::Application.config.secret_token = 'e7915f8c55719a3fc34cd838434b2ac58a9c322e05760624a7fa100ebc42c90831968ac12c660b2cba75c50ad6f378e13a62864916465da1337f9a4f858047f7'
